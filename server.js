6d0b92ebcf20982514b182ce80109d1f02b2b49a
alonzofroman <alonzofroman@gmail.com>
2021-10-19 19:05:03 -0700

    Loading notes from db.json

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const notesList = require('./db/db.json');
 
 const app = express();
 
@@ -20,7 +21,20 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
+// Load Notes
+app.get('/api/notes', (res, req) => {
+    fs.readFile(path.join(__dirname, './db/db.json'), (err, data) => {
+        if (err) {
+            throw err
+        }
+        let notes = JSON.parse(data);
+        req.json(notes);
+    });
+});
 
+// New Notes
+
+// Delete Notes
 
 
 // Port listener
