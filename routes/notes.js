ee3942c2db96176dc8d61d2bdd36664af2a7652d
alonzofroman <alonzofroman@gmail.com>
2021-10-20 21:41:33 -0700

    Added comments to router functions

--- routes/notes.js
+++ routes/notes.js
@@ -9,6 +9,7 @@ const { v4: uuidv4 } = require('uuid');
 
 // Load Notes
 router.get('/notes', (req, res) => {
+    // Read from notes in db.json
     console.info(`${req.method} request received to get notes`);
     fs.readFile(path.join(__dirname, '../db/db.json'), (err, data) => {
         if (err) {
@@ -22,17 +23,20 @@ router.get('/notes', (req, res) => {
 // New Notes
 router.post('/notes', (req, res) => {
     console.info(`${req.method} request received to add note`);
+    // Read notes from db.json
     fs.readFile(path.join(__dirname, '../db/db.json'), (err, data) => {
         if (err) {
             throw err
         }
         let notes = JSON.parse(data);
+        // Create new note with ids
         const { title, text} = req.body;
             const newNote = {
                 title,
                 text,
                 id: uuidv4(),
             }
+            // Push new note to notes and overwrite db.json with updated list
         notes.push(newNote);
         fs.writeFile('./db/db.json', JSON.stringify(notes), null, (err) => {
             if (err) {
@@ -47,17 +51,19 @@ router.post('/notes', (req, res) => {
 // Delete Note
 router.delete('/notes/:id', (req, res) => {
     console.info(`${req.method} request received to delete a note`);
-    
+    // Read notes from db.json
     fs.readFile(path.join(__dirname, '../db/db.json'), (err, data) => {
         if (err) {
             throw err
         }
     let notes = JSON.parse(data);
+    // Find the note that matches IDs to delete request and remove it
     for(i=0; i<notes.length; i++) {
         if (notes[i].id === req.params.id) {
              notes.splice(i, 1);
         }
     }
+    // Write updated notes list to db.json
     fs.writeFile('./db/db.json', JSON.stringify(notes), null, (err) => {
         if (err) {
             throw err
